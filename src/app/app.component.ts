216e9096bc366393c926f1c596cab416382f19d6
bibekstha123 <bibekstha084@gmail.com>
2019-02-03 20:33:18 +0545

    this is first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo';
+ isDisabled: boolean = false;
+
+ inputCls="textColo textSize";//[class] is used
+
+
+ message: string="hello";
+
+ contact={       //as an object, [ngClass] is used
+   "fontStyle": true,
+   "textColor": false
+ };
+
+ onSubmit(){
+     this.message = "heyy,,"
+ }
+
+ 
+ Products = [];
+
+ constructor(){
+   this.Products=[
+     {
+       sno:1,
+       product:"jacket"
+     },
+     {
+      sno:2,
+      product:"pants"
+    },
+   ]
+ }
+
 }
